a05e698c76a6c12f9ec9b2aaee6a1b8792b08f3d
shubham kumar <shubham.chumber@smu.ca>
2019-05-11 22:26:13 -0300

    Completed
    Initalized using spring initializer
    - ADDED Model classes - course and student
    - ADDED Controller
    - ADDED Service to retrieve student and courses

---
+++ student-WebServices/student-WebServices/src/main/java/com/shubham/springboot/studentWebServices/StudentWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.springboot.studentWebServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentWebServicesApplication.class, args);
+	}
+
+}
